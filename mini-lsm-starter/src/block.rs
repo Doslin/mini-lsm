42a0da9d402fb9f41b98b86ba30133f14c19220b
zhilin.qiu <zhilin.qiu@appshahe.com>
2024-02-05 17:24:15 +0800

    week1 day3 ok

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -5,10 +5,12 @@ mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes};
 pub use iterator::BlockIterator;
 
+pub(crate) const SIZEOF_U16: usize = std::mem::size_of::<u16>();
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
+/// 块是 LSM 树中读取和缓存的最小单位。它是已排序键值对的集合。
 pub struct Block {
     pub(crate) data: Vec<u8>,
     pub(crate) offsets: Vec<u16>,
@@ -18,11 +20,33 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        // 把数据复制出来
+        let mut buf = self.data.clone();
+        let offsets_len = self.offsets.len();
+        // 把偏移量写入
+        for offset in &self.offsets {
+            buf.put_u16(*offset)
+        }
+        buf.put_u16(offsets_len as u16);
+        // 先是数据，然后是偏移量，最后是偏移量的长度
+        buf.into()
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        // get number of elements in the block
+        // 偏移量的长度先取出来
+        let entry_offsets_len = (&data[data.len() - SIZEOF_U16..]).get_u16() as usize;
+        let data_end = data.len() - SIZEOF_U16 - entry_offsets_len * SIZEOF_U16;
+        // 取出所有的 offset
+        let offset_raw = &data[data_end..data.len() - SIZEOF_U16];
+        // get offset array
+        let offsets = offset_raw
+            .chunks(SIZEOF_U16)
+            .map(|mut x| x.get_u16())
+            .collect();
+        // retrieve data
+        let data = data[0..data_end].to_vec();
+        Self { data, offsets }
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,9 +1,8 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+use bytes::BufMut;
 
 use crate::key::{KeySlice, KeyVec};
 
-use super::Block;
+use super::{Block, SIZEOF_U16};
 
 /// Builds a block.
 pub struct BlockBuilder {
@@ -20,22 +19,56 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
+    }
+    fn estimated_size(&self) -> usize {
+        SIZEOF_U16 /* number of key-value pairs in the block */ +  self.offsets.len() * SIZEOF_U16 /* offsets */ + self.data.len()
+        // key-value pairs
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        // key-value pairs
+        assert!(!key.is_empty(), "key must not be empty");
+        if self.estimated_size() + key.len() + value.len() + SIZEOF_U16 * 3 /* key_len, value_len and offset */ > self.block_size
+            && !self.is_empty()
+        {
+            return false;
+        }
+        // 把数据长度放到偏移量数组中，
+        // 有个专门的偏移量数组，用来存储每个 key-value 对的偏移量。而 key-value 就存储在一起，都是在 data 中。都是先写入偏移量，然后写入数据。
+        // Add the offset of the data into the offset array.
+        self.offsets.push(self.data.len() as u16);
+        // Encode key length.
+        self.data.put_u16(key.into_inner().len() as u16);
+        // Encode key content.
+        self.data.put(key.into_inner());
+        // Encode value length.
+        self.data.put_u16(value.len() as u16);
+        // Encode value content.
+        self.data.put(value);
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        if self.is_empty() {
+            panic!("block should not be empty");
+        }
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
